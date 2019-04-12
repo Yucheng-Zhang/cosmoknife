7fd3298ae6511ce6aa3166f86e05f52345721a0b
Yucheng Zhang <yczhang16807@icloud.com>
2019-04-11 20:52:50 -0400

    fix

--- knifemask.py
+++ knifemask.py
@@ -13,7 +13,7 @@ def get_ra_dec(theta, phi):
     theta_equ, phi_equ = rot(theta, phi)
     dec, ra = 90. - np.rad2deg(theta_equ), np.rad2deg(phi_equ)
     # move RA in [-180,0) to [180,360)
-    ra = np.where(ra < 0., ra + 180., ra)
+    ra = np.where(ra < 0., ra + 360., ra)
 
     return ra, dec
 
