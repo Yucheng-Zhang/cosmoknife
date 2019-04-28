ad6d4ded599949e62a1459575ab3e65d40b016be
Yucheng Zhang <yczhang16807@icloud.com>
2019-04-27 23:06:27 -0400

    fix

--- label.py
+++ label.py
@@ -57,7 +57,7 @@ def label_bounds(data, jkr, ll):
 def save_data(data, fn):
     '''Save the output data with jk label.'''
     header = 'RA   DEC   redshift   weight   jackknife'
-    fmt = '%.15e   %.15e   %.15e   %.15e   %-8d'
+    fmt = '%>2.15e   %>2.15e   %>2.15e   %>2.15e   %>8d'
     np.savetxt(fn, data, fmt=fmt, header=header)
     print(':: Data written to file: {}'.format(fn))
 
@@ -114,7 +114,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print('>> Loading file: {}'.format(args.data))
     data = mf.load_data_pd(args.data)
 
     if args.tp == 'map':
